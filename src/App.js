ff4cca6bc275cf0def03e581ad4fa5c3507a3e83
Harmony <bluedonice@gmail.com>
2023-01-12 21:10:59 +0100

    uncomment gitignore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Products, Navbar } from './components/';
+import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path={"/products"} element={<Products />}/>
+      </Routes>
     </div>
   );
 }
-
 export default App;
